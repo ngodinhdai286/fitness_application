e25a53965b1e18046fad1ed73a975f4753c182bd
NGO DINH DAI <ngodinhdai77@gmail.com>
2023-04-19 16:26:00 +0700

    our classes section

---
+++ src/scenes/ourclasses/Class.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type Props = { name: string; description?: string; image: string };
+
+function Class({ name, description = "Some description", image }: Props) {
+  const overlayStyles = `p-5 absolute z-30 flex h-[380px] w-[450px] flex-col items-center justify-center whitespace-normal bg-primary-500 text-center text-white opacity-0 transition duration-500 hover:opacity-90`;
+
+  return (
+    <li className="scrollbar-custom relative mx-5 inline-block h-[380px] w-[450px]">
+      <div className={overlayStyles}>
+        <p className="text-2xl">{name}</p>
+        <p className="mt-5">{description}</p>
+      </div>
+      <img alt={`${image}`} src={image} />
+    </li>
+  );
+}
+
+export default Class;
